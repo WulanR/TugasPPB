83c57e70806cee0d97cf1622dae19ee6da07450f
Wulan Rohmadani <wulanr161299@gmail.com>
2016-09-13 20:27:05 +0700

    first commit : hanya tampilan saja

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4033/tugasppb/MainActivity.java
@@ -0,0 +1,17 @@
+package id.sch.smktelkom_mlg.tugas01.xirpl4033.tugasppb;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+
+
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
